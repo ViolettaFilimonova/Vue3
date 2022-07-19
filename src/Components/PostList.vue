1ae22a844a98b310503e313829d5b5c65878cd52
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-07-19 21:44:56 +0300

    animations

--- src/Components/PostList.vue
+++ src/Components/PostList.vue
@@ -1,15 +1,16 @@
 <template>
   <div v-if="posts.lenght !== 0">
-    <h3>Список пользователей</h3>
-    <post-item 
-      v-for="post in posts"
-      :post="post"
-      key="post.id" 
-      class="post" 
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          class="post"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
-  <div class="red" v-else>
+  <div class="red" v-else-if="posts.lenght === 0">
     <h2>Список постов пуст</h2>
   </div>
 </template>
@@ -28,10 +29,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="css">
 .red{
   color: red;
   display: flex;
   justify-content: center;
+}
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active, .post-list-leave-active {
+  transition: all 1s ease;
+}
+.post-list-enter-from, .post-list-leave-to{
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move{
+  transition: transform 0.4s ease;
 }
 </style>
